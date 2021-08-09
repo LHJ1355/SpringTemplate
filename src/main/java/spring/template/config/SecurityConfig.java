16a14ea626d1c36d528a0501ebc67f5802270e20
LHJ1355 <lhj56411355@gmail.com>
2021-08-09 23:55:24 +0900

    basic

---
+++ src/main/java/spring/template/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package spring.template.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "/login", "/signup").permitAll();
+        super.configure(http);
+    }
+}
